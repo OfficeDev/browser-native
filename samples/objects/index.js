9d068bc5c76f38e7929c033a9f8f683ac15461be
Bob German <bgerman1@hotmail.com>
2022-04-12 21:29:36 -0400

    First sample

--- samples/objects/index.js
+++ samples/objects/index.js
@@ -1,1 +1,45 @@
-alert('Hi');
+(() => {
+    
+    class Person {
+        
+        // Private fields
+        #firstName;
+        #lastName;
+        
+        constructor (firstName, lastName) {
+            this.#firstName = firstName;
+            this.#lastName = lastName;
+        }
+        
+        getName() {
+            return this.#firstName + " " + this.#lastName;
+        }
+    }
+
+    class Geek extends Person {
+        
+        #favoriteDevLanguage;
+        #favoriteSciFiMovie;
+        
+        constructor (firstName, lastName, favoriteDevLanguage,
+                     favoriteSciFiMovie)
+                     {
+                         super(firstName, lastName);
+                         this.#favoriteDevLanguage = favoriteDevLanguage;
+                         this.#favoriteSciFiMovie = favoriteSciFiMovie;
+                     }
+                     
+        getMessage() {
+            return super.getName() + " likes " + me.#favoriteDevLanguage + " and " + me.#favoriteSciFiMovie;
+        }
+                
+    }
+
+    var me = new Geek ("Bob", "German", "TypeScript", "Forbidden Planet");
+
+    var result = me.getMessage() 
+
+    document.getElementById('output').innerText = result;
+    
+})();
+    
