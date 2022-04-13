0ca8056f072688089551ebdc539fa91994c8f410
Bob German <bgerman1@hotmail.com>
2022-04-13 14:06:14 -0400

    Listings page with details in json file

---
+++ listing/index.js
@@ -0,0 +1,36 @@
+'use strict';
+(async () => {
+
+    const params = new URLSearchParams(window.location.search);
+    const sample = params.get('sample');
+
+    const sampleUrl = `${window.location.origin}${window.location.pathname}${sample}/`
+        .replace('/listing/', '/samples/');
+
+    const response = await fetch(`${sampleUrl}details.json`);
+    const details = await (response.json());
+
+    document.getElementById('title').innerText =
+        `browser native | ${details.title.toLowerCase()}`;
+    document.getElementById('description').innerHTML = details.description;
+
+    const embed = details.files.map(f => ({
+        "type": f.split('.').pop(),
+        "label": f,
+        "path": `samples/${sample}/${f}`
+    }));
+    embed.push({
+        "type": "htmlpage",
+        "label": "result",
+        "url": `${sampleUrl}`
+    });
+
+    window.githubEmbed('#listings', {
+        "owner": "OfficeDev",
+        "repo": "browser-native",
+        "ref": "gh-pages",
+        "embed": embed
+    });
+
+})();
+
