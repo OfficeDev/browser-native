98442ad6ce9853e7adf495f0b13d6d714fc265c8
Bob German <bgerman1@hotmail.com>
2022-04-15 15:22:27 -0400

    Fix up readme and Classes sample

--- samples/classes/index.js
+++ samples/classes/index.js
@@ -18,28 +18,32 @@
 
     class Geek extends Person {
         
+        // Private fields
         #favoriteDevLanguage;
         #favoriteSciFiMovie;
         
         constructor (firstName, lastName, favoriteDevLanguage,
                      favoriteSciFiMovie)
                      {
+                         // The base class is called "super"
                          super(firstName, lastName);
                          this.#favoriteDevLanguage = favoriteDevLanguage;
                          this.#favoriteSciFiMovie = favoriteSciFiMovie;
                      }
                      
         getMessage() {
-            return super.getName() + " likes " + me.#favoriteDevLanguage + " and " + me.#favoriteSciFiMovie;
+            return super.getName() + " likes " + this.#favoriteDevLanguage + " and " + this.#favoriteSciFiMovie;
         }
                 
     }
 
-    var me = new Geek ("Bob", "German", "TypeScript", "Forbidden Planet");
+    var bob = new Geek ("Bob", "German", "C#", "The Matrix");
+    var julie = new Geek ("Julie", "Turner", "???", "???");
 
-    var result = me.getMessage() 
-
-    document.getElementById('output').innerText = result;
+    document.getElementById('output').innerHTML = `
+    <p>${bob.getMessage()}</p>
+    <p>${julie.getMessage()}</p>
+    `;
     
 })();
     
