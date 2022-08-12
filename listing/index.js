9c9bf2346dd716f8bec0ebadd147e26b5b46b832
Bob German <bgerman1@hotmail.com>
2022-08-12 11:04:42 -0400

    Improve nav to subsamples

--- listing/index.js
+++ listing/index.js
@@ -14,6 +14,27 @@
         ` | ${details.title.toLowerCase()}`;
     document.getElementById('description').innerHTML = details.description;
 
+    let navHtml = "";
+    if (details.nav && details.nav.length > 1) {
+        navHtml = "Samples in this set: ";
+        let i = 1;
+        details.nav.forEach((e) => {
+            if (sample === e) {
+                if (i > 1) {
+                    document.getElementById('title').innerText += " " + i;
+                }
+                navHtml += `<b>${i++}</b>&nbsp;`;
+            } else {
+                navHtml += `<a href="${window.location.href.replace(sample, e)}">${i++}</a>&nbsp;`;
+            }
+        })
+    }
+    document.getElementById('nav').innerHTML = navHtml;
+
+    if (details.subtitle) {
+        document.getElementById('title').innerText += " - " + details.subtitle;
+    }
+
     const embed = details.files.map(f => ({
         "type": f.split('.').pop(),
         "label": f,
