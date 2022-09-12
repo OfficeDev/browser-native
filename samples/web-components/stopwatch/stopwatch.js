a194bd5f1fa720bf7954bd64a58f8193cef92dcd
Bob German <bgerman1@hotmail.com>
2022-09-11 20:54:51 -0400

    Now stops, resets, starts

--- samples/web-components/stopwatch/stopwatch.js
+++ samples/web-components/stopwatch/stopwatch.js
@@ -1,33 +1,46 @@
 import stylesheet from './style.css' assert { type: 'css' };
-const BUTTON_KEY_PREFIX = 'stateful-button';
-
 
 const TICK_INTERVAL = 1000;
 
 export class stopwatch extends HTMLElement {
 
     // Component state is in these fields:
-    #button;
-    #currentTime = 0;
+    #buttonElement;
+    #startTime = Date.now();
+    #running = false;
 
     constructor() {
         super();
 
         const shadowRoot = this.attachShadow({mode: 'open'});
         shadowRoot.adoptedStyleSheets = [ stylesheet ];
-        this.#button = document.createElement('button');
-        this.#button.innerText = this.#formatTime(this.#currentTime);
 
-        shadowRoot.appendChild(this.#button);
+        this.#buttonElement = document.createElement('button');
+        this.#buttonElement.innerText = this.#formatTime(0);
+        shadowRoot.appendChild(this.#buttonElement);
 
         setInterval(() => {
-            this.#currentTime += TICK_INTERVAL;
-            this.#button.innerText = this.#formatTime(this.#currentTime);
+            if (this.#running) {
+                const currentTime = Date.now() - this.#startTime;
+                this.#buttonElement.innerText = this.#formatTime(currentTime);    
+            }
         }, TICK_INTERVAL);    
     }
 
     #clickHandler() {
-        this.#button.innerText = "You clicked me";
+        if (!this.#running) {
+            if (this.#buttonElement.innerHTML === this.#formatTime(0)) {
+                // Watch was already reset; start it now
+                this.#startTime = Date.now();
+                this.#running = true;
+            } else {
+                // Reset the watch to 0
+                this.#buttonElement.innerHTML = this.#formatTime(0);
+            }
+        } else {
+            // Stop the watch
+            this.#running = false;
+        }
     }
 
     // Take a moment and avoid a dependency
