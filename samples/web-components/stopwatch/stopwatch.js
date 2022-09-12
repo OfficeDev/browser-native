18f949f50c6a6b0fe1dc2cd03960ed1d05252d88
Bob German <bgerman1@hotmail.com>
2022-09-11 20:32:33 -0400

    It ticks!

--- samples/web-components/stopwatch/stopwatch.js
+++ samples/web-components/stopwatch/stopwatch.js
@@ -1,6 +1,9 @@
 import stylesheet from './style.css' assert { type: 'css' };
 const BUTTON_KEY_PREFIX = 'stateful-button';
 
+
+const TICK_INTERVAL = 1000;
+
 export class stopwatch extends HTMLElement {
 
     // Component state is in these fields:
@@ -13,18 +16,30 @@ export class stopwatch extends HTMLElement {
         const shadowRoot = this.attachShadow({mode: 'open'});
         shadowRoot.adoptedStyleSheets = [ stylesheet ];
         this.#button = document.createElement('button');
-        this.#button.innerText = this.#currentTime;
+        this.#button.innerText = this.#formatTime(this.#currentTime);
+
         shadowRoot.appendChild(this.#button);
+
+        setInterval(() => {
+            this.#currentTime += TICK_INTERVAL;
+            this.#button.innerText = this.#formatTime(this.#currentTime);
+        }, TICK_INTERVAL);    
     }
 
     #clickHandler() {
         this.#button.innerText = "You clicked me";
     }
 
+    // Take a moment and avoid a dependency
+    #formatTime(milliseconds) {
+        const date = new Date(0);
+        date.setMilliseconds(milliseconds);
+        return date.toISOString().substring(11,19);
+    }
+
     async connectedCallback() {
 
         this.onclick = this.#clickHandler;
-        this.render();
 
     }
 }
