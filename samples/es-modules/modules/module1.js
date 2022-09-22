1f7c7a57f4aa79d01273cb0acace238b05ec9283
Bob German <bgerman1@hotmail.com>
2022-09-22 15:44:29 -0400

    Add ES Modules

---
+++ samples/es-modules/modules/module1.js
@@ -0,0 +1,7 @@
+// 1. Multiple exports
+
+export const line = "Twas bryllyg, and the slythy toves";
+
+export function getLine () {
+    return "Did gyre and gymble in the wabe:"
+}
---
+++ samples/es-modules/modules/module2.js
@@ -0,0 +1,7 @@
+// 2. Default export
+
+export default class LineProviderService {
+    getLine () {
+        return "All mimsy were ye borogoves;";
+    }
+}
